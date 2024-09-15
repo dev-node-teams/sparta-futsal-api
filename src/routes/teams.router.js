4c93326b9065a064ad3de81ef5563a61cf6119f2
devbong92 <web.bong92@gmail.com>
2024-09-15 23:36:07 +0900

    Feat: 팀 선발 해제 기능 구현
    팀 선발 해제기능 API 개발
    
    Ref: #9

---
+++ src/routes/teams.router.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import joi from 'joi';
+import asyncHandler from 'express-async-handler';
+import { prisma } from '../utils/prisma/index.js';
+
+const router = express.Router();
+
+/** 선발 해제 API */
+router.delete(
+  '/teams/starting',
+  asyncHandler(async (req, res, next) => {
+    const joiSchema = joi.object({
+      playerId: joi.number().required().messages({
+        'number.base': '플레이어 아이디는 숫자타입이어야 합니다.',
+        'any.required': '플레이어 아이디를 입력해주세요.',
+      }),
+    });
+
+    const validation = await joiSchema.validateAsync(req.body);
+    const { playerId } = validation;
+
+    // @TODO: TEST코드 삭제예정
+    const userId = 1;
+
+    // 대상 선수 조회
+    const targetPlayer = await prisma.usersPlayers.findFirst({
+      where: {
+        playerId: playerId,
+        userId: userId,
+      },
+    });
+
+    if (!targetPlayer) {
+      // 대상 선수가 없는 경우,
+      throw new Error('해당 선수 조회에 실패하였습니다.');
+    } else if (!targetPlayer.startingLine) {
+      // 이미 선발이 아닌 경우,
+      throw new Error('해당 선수는 선발선수가 아닙니다.');
+    }
+
+    // 선발 여부 변경
+    const updatePlayer = await prisma.usersPlayers.update({
+      data: {
+        startingLine: false,
+      },
+      where: {
+        userPlayerId: targetPlayer.userPlayerId,
+      },
+      select: {
+        user: {
+          select: { nickname: true },
+        },
+        player: {
+          select: { playerName: true },
+        },
+      },
+    });
+
+    return res.status(200).json({
+      messages: `[${updatePlayer.user.nickname}]의 [${updatePlayer.player.playerName}] 선수를 선발에서 해제하였습니다.`,
+    });
+  }),
+);
+
+export default router;
