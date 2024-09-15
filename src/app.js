de82aae55a9d9c72bb4077a59104f712b84da88c
devbong92 <web.bong92@gmail.com>
2024-09-15 23:05:04 +0900

    Feat: prisma 세팅 추가

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+// .env => process.env
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// app.use('/api', []);
+
+app.listen(PORT, () => {
+  console.log(PORT, '포트로 서버가 열렸어요!');
+});
