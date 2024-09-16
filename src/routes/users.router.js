a70bcca9637105471b1e377fcf53f2748fd81aec
NSHkw <64311828+NSHkw@users.noreply.github.com>
2024-09-16 15:59:06 +0900

    add joi validation, change cookie to header

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -2,12 +2,37 @@ import express from 'express';
 import { prisma } from '../utils/prisma/index.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import joi from 'joi';
 
 const UsersRouter = express.Router();
 
 // 로그인 API
 UsersRouter.post('/users/sign-in', async (req, res, next) => {
   const { email, password } = req.body;
+
+  const userVal = joi.object({
+    email: joi
+      .string()
+      .pattern(/^[a-zA-Z0-9]{6,20}$/)
+      .required()
+      .message({
+        'string.pattern.base': '이메일은 6~20자의 영문, 숫자로 이루어져야 한다.',
+        'string.empty': '이메일은 반드시 작성해야 한다.',
+      }),
+    password: joi
+      .string()
+      .pattern(/^[!@#$%^&*a-zA-Z0-9]{6,20}$/)
+      .required()
+      .message({
+        'string.pattern.base': '패스워드는 6~20자의 영문, 숫자, 특수문자로 이루어져야 한다.',
+        'string.empty': '패스워드는 반드시 작성해야 한다.',
+      }),
+  });
+
+  const error = userVal.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: '유효하지 않은 값' });
+  }
 
   try {
     const loginUser = await prisma.users.findUnique({
@@ -35,7 +60,7 @@ UsersRouter.post('/users/sign-in', async (req, res, next) => {
       { expiresIn: '1h' },
     );
 
-    res.cookie('authorization', `Bearer ${token}`);
+    res.setHeader('Authorization', `Bearer ${token}`);
 
     return res.status(200).json({ message: '로그인 성공' });
   } catch (error) {
