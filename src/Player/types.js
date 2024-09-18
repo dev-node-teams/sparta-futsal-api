4028b02122d9a5bda0a3f9f3849523de48f0ed29
조정현 <emforhs0913@naver.com>
2024-09-18 17:09:45 +0900

    feat: players.router, PlayerManager
    [요약]
    -/src/routes에 players.router.js추가
    -/src/Player디렉토리 추가
    │
    └─PlayerManager.js추가
    └─types.js추가
    
    [PlayerManager]
    -서버 실행 시, DB에서 모든 선수 정보를 가져와 Map자료구조에 저장
    -모든 매소드와 멤버 변수는 static
    -선수 정보 요청 시, 인자로 받은 선수id를 key값으로 사용하여 value 반환
    [players.router]
    -'/:player_id'를 통해 get요청이 들어오면 PlayerManager의 getPlayerOrNull()를 호출하여 선수의 정보를 반환
    Resolves: #11

---
+++ src/Player/types.js
@@ -0,0 +1,10 @@
+export class Player {
+    constructor(player_name, speed, finishing, shotPower, defense, stamina) {
+        this.player_name = player_name;
+        this.speed = speed;
+        this.finishing = finishing;
+        this.shotPower = shotPower;
+        this.defense = defense;
+        this.stamina = stamina;
+    }
+}
