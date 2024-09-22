cf1384d62c0c3a82df27df861696c7e6fc46c12f
조정현 <emforhs0913@naver.com>
2024-09-23 01:59:28 +0900

    Feat: Utils 기능 추가
    1. add: getAdjustLevelOVR()
    2. add: getUpgradePercent()
    3. add: clamp()
    4. bugfix: 무작위 선수 뽑기
    
    1. getAdjustLevelOVR(OVR, level): OVR에 level보정치를 더한 값을 반환
    2. getUpgradePercent(upgradeOVR, materialOVR): 강화할 선수의 OVR과 재료의 OVR를 비교하여 성공할 확률 반환
    3. clamp(val, min, max): val값이 최소 min, 최대 max사이의 값을 갖게 보장
    4. DB에 등급 속성 추가에 따른 가중치 랜덤 뽑기 정상화
    
    about: #25, #5

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,12 @@
 export class Utils{
 /*---------------------------------------------
+    [개요]
+    1. drawPlayer(players): 무작위 선수 뽑기
+    2. getAdjustLevelOVR(OVR, level): OVR에 level보정치를 더한 값을 반환
+    3. getUpgradePercent(upgradeOVR, materialOVR): 강화할 선수의 OVR과 재료의 OVR를 비교하여 성공할 확률 반환
+    4. clamp(val, min, max): val값이 최소 min, 최대 max사이의 값을 갖게 보장
+
+/*---------------------------------------------
     [무작위 선수 뽑기]
 
     1. 가중치가 담긴 player배열 넘겨받기
@@ -9,13 +16,12 @@ export class Utils{
 
     TODO: 
         1. 가중치 추가하기(등급을 추가하기)
-
 ---------------------------------------------*/
     static drawPlayer(players) {
         //2. 가중치 총합 구하기
         let totalWeight = 0;
         for(let i = 0; i < players.length; i += 1){
-            totalWeight += players[i].weight;
+            totalWeight += players[i].rarity;
         }
 
         //3. [0, 가중치 총합)의 난수 구하기
@@ -26,13 +32,72 @@ export class Utils{
     
         //4. 가중치 누적합 계산 및 선수 반환
         for (const player of players) {
-            //weightSum += player.weight;
-            //if (randomNum < weightSum) {
-            if (true) {
+            weightSum += player.rarity;
+            if (randomNum < weightSum) {
                 return player;
             }
         }
 
         return null
+    }
+/*---------------------------------------------
+    [레벨 별 능력치 부여]
+---------------------------------------------*/
+    static getAdjustLevelOVR(OVR, level){
+        console.log(OVR+" : "+level);
+        switch(level){
+            case 0: return OVR;
+            case 1: return OVR+3;
+            case 2: return OVR+4;
+            case 3: return OVR+5;
+            case 4: return OVR+6;
+            case 5: return OVR+8;
+            case 6: return OVR+10;
+            case 7: return OVR+12;
+            case 8: return OVR+15;
+            case 9: return OVR+18;
+            case 10: return OVR+22;
+            default:
+                console.log("sl");
+                return null;
+        }
+    }
+/*---------------------------------------------
+    [강화 성공 확률]
+---------------------------------------------*/
+    static getUpgradePercent(upgradeOVR, materialOVR){
+        if(upgradeOVR == materialOVR)
+            return 0.5;
+        else if(upgradeOVR == materialOVR+1)
+            return 0.67;
+        else if(upgradeOVR == materialOVR+2)
+            return 0.9;
+        else if(upgradeOVR <= materialOVR+3)
+            return 1;
+        else if(upgradeOVR == materialOVR-1)
+            return 0.373;
+        else if(upgradeOVR == materialOVR-2)
+            return 0.275;
+        else if(upgradeOVR == materialOVR-3)
+            return 0.208;
+        else if(upgradeOVR == materialOVR-4)
+            return 0.152;
+        else if(upgradeOVR == materialOVR-5)
+            return 0.113;
+        else if(upgradeOVR == materialOVR-6)
+            return 0.084;
+        else if(upgradeOVR == materialOVR-7)
+            return 0.062;
+        else if(upgradeOVR == materialOVR-8)
+            return 0.046;
+        else
+            return 0.035
+    }
+
+/*---------------------------------------------
+    [clamp]
+---------------------------------------------*/
+    static clamp(val, min, max){
+        return Math.min(Math.max(val, min), max);
     }
 }
